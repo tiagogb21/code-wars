aa32f81cd4bd0007a7178bc024e1b5b7b0a313b6
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2021-10-21 20:04:05 -0300

    add counter-of-neighbor-ones.js

---
+++ katas 7/Counter-of-neighbor-ones.js
@@ -0,0 +1,18 @@
+function onesCounter(input) {
+  let count = 0;
+  let output = [];
+  for (let index = 0; index < input.length; index++) {
+    if (input[index] === 1) {
+      while (input[index] === 1) {
+        count++;
+        index++;
+      }
+      output.push(count);
+    }
+    count = 0;
+  }
+  if (output.length === 0) {
+    return [];
+  }
+  return output;
+}
